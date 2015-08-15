ddedd8583fdc13887d123e2d747f588c7eba7c4e
Rafael Rocha <rafaelrochasilva@gmail.com>
2015-08-14 23:46:47 -0300

    Refactor Player class by changing method names, and refactoring code

--- js/player.js
+++ js/player.js
@@ -1,42 +1,60 @@
 Game.Player = (function() {
   var VELOCITY = 100;
+  var JUMP_HEIGHT = -300;
+  var VERTICAL_GRAVITY = 500;
 
   function Player(name, game) {
     this.name = name;
     this.game = game;
+    this.obj = this.initPlayer();
 
-    this.cursor = this.game.input.keyboard.createCursorKeys();
+    this.initCursors();
+    this.eventListeners();
+  }
 
-    this.player = this.game.add.sprite(
+  var fn = Player.prototype;
+
+  fn.eventListeners = function() {
+    this.cursorUP.onDown.add(this.moveUp, this);
+  };
+
+  fn.initPlayer = function() {
+    var player = this.game.add.sprite(
       this.game.world.centerX,
       this.game.world.centerY,
       'player'
     );
 
     // Centralize player related to its position
-    this.player.anchor.setTo(1,1);
-
-    // Add physics
-    this.game.physics.arcade.enable(this.player);
+    player.anchor.setTo(1,1);
 
     // Add vertical gravity
-    this.player.body.gravity.y = 500;
-  }
+    this.game.physics.arcade.enable(player);
+    player.body.gravity.y = VERTICAL_GRAVITY;
 
-  var fn = Player.prototype;
+    return player;
+  };
 
-  fn.move = function() {
-    if (this.cursor.left.isDown) {
-      this.player.body.velocity.x = -VELOCITY;
+  fn.initCursors = function() {
+    this.cursor = this.game.input.keyboard.createCursorKeys();
+    this.cursorUP = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
+  };
+
+  fn.moveUp = function() {
+    if(this.obj.body.touching.down) {
+      this.obj.body.velocity.y = JUMP_HEIGHT;
+    }
+  };
+
+  fn.movementListener = function() {
+    if(this.cursor.left.isDown) {
+      this.obj.body.velocity.x = -VELOCITY;
     }
     else if(this.cursor.right.isDown) {
-      this.player.body.velocity.x = VELOCITY;
-    }
-    else if(this.cursor.up.isDown && this.player.body.touching.down) {
-      this.player.body.velocity.y = -320;
+      this.obj.body.velocity.x = VELOCITY;
     }
     else {
-      this.player.body.velocity.x = 0;
+      this.obj.body.velocity.x = 0;
     }
   };
 
